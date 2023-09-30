95f12a9fd35222c0435b9807ed3a8b930c83ccf5
Luiz Filipe Aquino <FilipeProfissional11@gmail.com>
2023-09-30 16:33:53 -0300

    Projeto-inicio
    preparação para o projeto, excluindo alguns comentários e arquivos;
    Instalando os ícones que serão usados;

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,17 +1,8 @@
 # vamos deixar as coisas mais organizadas:
-from django.contrib import admin
 from django.urls import path
-from recipes.views import home, sobre
+from recipes.views import home
 
 
 urlpatterns = [
     path('', home),  # pagina raiz(home)
-    path('admin/', admin.site.urls),
-    path('sobre/', sobre)  # 1. (/sobre/)
 ]
-
-# Porta de entrada da aplicação. Vamos conseguir indicar os caminhos
-# que cada url vai tomar usando apps
-# 1. eu quero um caminho que aponte para algun lugar usando o path('algo/'),
-# por enquanto n vai ver nada e precisa dos outros parametros.
-# o view tem que receber como argumento o request e retornar um reponse
--- recipes/views.py
+++ recipes/views.py
@@ -5,14 +5,6 @@ from django.shortcuts import render
 def home(request) -> None:
     """ função sobre """
     #            request, arquivo
-    return render(request, 'recipes/home.html')
-
-
-def sobre(request) -> None:
-    """ função sobre """
-    return render(request, 'recipes/sobre.html')
-
-# criamos essa aplicação(app) chamada recipes para config as views dessa pagina
-# quando um cliente faz um get ele quer receber uma pagina e é mto
-# desnecessario escrever tudo na mão, por isso vamos usar o render para
-# rederizar um arquivo html
+    return render(request, 'recipes/home.html', status=200, context={
+        "name": "Luiz filipe"
+        })
