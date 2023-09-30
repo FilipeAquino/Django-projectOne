a21c23ae7d7ca09fe4899d0305d1bcd91bd4faa1
Luiz Filipe Aquino <FilipeProfissional11@gmail.com>
2023-09-30 15:04:10 -0300

    Templates
    Falando um poucos dos templates dentro de recipes; e como fazer o django reconhece-lo

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,18 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
 
 
 def home(request) -> None:
     """ função sobre """
-    return HttpResponse("ola vc esta no home")
+    #            request, arquivo
+    return render(request, 'recipes/home.html')
 
 
 def sobre(request) -> None:
     """ função sobre """
-    return HttpResponse("ola vc esta no sobre")
+    return render(request, 'recipes/sobre.html')
 
 # criamos essa aplicação(app) chamada recipes para config as views dessa pagina
+# quando um cliente faz um get ele quer receber uma pagina e é mto
+# desnecessario escrever tudo na mão, por isso vamos usar o render para
+# rederizar um arquivo html
